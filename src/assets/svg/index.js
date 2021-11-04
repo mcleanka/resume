44d491a79c16c15bedc7f0aaac5c232785cef668
mcleanka <mcleankasambala@gmail.com>
2021-11-04 21:48:37 +0200

    Added resume files

---
+++ src/assets/svg/index.js
@@ -0,0 +1,13 @@
+import SvgEducation from "./education.svg";
+import SvgWorkHistory from "./work-history.svg";
+import SvgProgramminSkills from "./programming-skills.svg";
+import SvgProjects from "./projects.svg";
+import SvgInterests from "./interests.svg";
+
+export {
+	SvgEducation,
+	SvgWorkHistory,
+	SvgProgramminSkills,
+	SvgProjects,
+	SvgInterests
+}
