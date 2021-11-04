7bfe1921df66003ae19ba2a3c75a43514d50e474
mcleanka <mcleankasambala@gmail.com>
2021-11-04 22:17:07 +0200

    Updated components

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,17 @@
 import Introduction from "./Introduction";
 import Services from "./Services";
 import About from "./About";
-import Skills from "./Skills";
-import Portfolio from "./Portfolio";
-import Blog from "./Blog";
 import Contact from "./Contact";
 import Statistics from "./Statistics";
-import Testimonial from "./Testimonial";
 import Partners from "./Partners";
-import Pricing from "./Princing";
 import Resume from "./Resume/Resume";
 
 export {
 	Introduction,
 	Services,
 	About,
-	Skills,
-	Portfolio,
-	Blog,
 	Contact,
 	Statistics,
-	Testimonial,
 	Partners,
-	Pricing,
 	Resume
 }
